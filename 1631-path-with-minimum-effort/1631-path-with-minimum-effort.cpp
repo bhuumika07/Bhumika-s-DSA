791291f4611aac17190d3d8056b7f4496511b9ae
bhuumika07 <bhumikaa2005@gmail.com>
2026-08-13 09:12:26 +0530

    Time: 39 ms (92.83%), Space: 24.4 MB (77.1%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -3,46 +3,42 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
 
         int rows[]={-1,0,1,0};
-        int cols[]={0,-1,0,1};
-        int rsize = heights.size();
-        int csize = heights[0].size();
-        // int mini=INT_MAX;
-        priority_queue< pair<int, pair<int,int> > , vector<pair<int,pair<int,int>>> , greater<pair<int , pair<int,int>>>>pq;
-        pq.push( {0, {0,0}});
-        vector<vector<int>>visited(rsize, vector<int>(csize, INT_MAX));
-        visited[0][0] = 0;
+        int cols[] ={0,-1,0,1};
+        int rsize=heights.size();
+        int csize=heights[0].size();
+        vector<vector<int>>reached(rsize, vector<int>(csize,1e9));
+        priority_queue< pair<int,pair<int,int>> , vector<pair<int,pair<int,int>>> , greater<pair<int,pair<int,int>>>>pq;
+        reached[0][0]=0;
+        pq.push({0,{0,0}});
         while(!pq.empty())
         {
             int val = pq.top().first;
             int r1 = pq.top().second.first;
             int c1= pq.top().second.second;
             pq.pop();
-            if( val > visited[r1][c1]) continue;
-            if( r1 == rsize-1 && c1 == csize-1) return val;
+
+            if(reached[r1][c1] < val ) continue;
+            if( reached[rsize-1][csize-1] < val) return reached[rsize-1][csize-1];
+            
             for( int i=0; i<4;i++)
             {
                 int nrow = r1 + rows[i];
-                int ncol = c1+ cols[i];
+                int ncol = c1 + cols[i];
 
-                if( nrow>=0 && nrow<rsize && ncol >=0 && ncol < csize)
+                if(nrow >= 0 && nrow <rsize && ncol >=0 && ncol <csize)
                 {
-                //    =abs( heights[nrow][ncol] - heights[r1][c1]);
                     int v = abs( heights[nrow][ncol] - heights[r1][c1]);
-                    v=max( v,val);
-                    if(v < visited[nrow][ncol]){
-                         visited[nrow][ncol]=v;
-                
-                        pq.push({v,{nrow,ncol}});
+                    v=max(v,val);
+                    if( reached[nrow][ncol] > v )
+                    {
+                        reached[nrow][ncol]=v;
+                        pq.push( {v , {nrow,ncol}});
                     }
-                    // if( nrow == rsize-1 && ncol == csize-1) mini=min(mini,v);
                 }
             }
-
         }
-        // if( mini ==INT_MAX) return 0;
-        return 0;
-
-        
+        if( reached[rsize-1][csize-1] == 1e9) return 0;
+        return reached[rsize-1][csize-1];
         
     }
 };
