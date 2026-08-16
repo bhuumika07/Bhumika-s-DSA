ca53b830e5eaccbcb38afe9b1f284cbd7f59aee6
bhuumika07 <bhumikaa2005@gmail.com>
2026-08-16 15:52:44 +0530

    Time: 6 ms (64.68%), Space: 35.5 MB (63.24%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -3,8 +3,8 @@ public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
         int rsize=maze.size();
         int csize=maze[0].size();
-        vector<vector<int>>reached(rsize, vector<int>(csize,INT_MAX) );
-        priority_queue<pair<int,pair<int,int>> , vector<pair<int,pair<int,int>>> , greater<pair<int,pair<int,int>>>>pq;
+       
+        queue<pair<int,pair<int,int>>>pq;
         pq.push({0,{entrance[0] , entrance[1]}});
 
         int rows[]={0,-1,0,1};
@@ -12,9 +12,9 @@ public:
 
         while(!pq.empty())
         {
-            int stops = pq.top().first;
-            int r1 = pq.top().second.first;
-            int c1 = pq.top().second.second;
+            int stops = pq.front().first;
+            int r1 = pq.front().second.first;
+            int c1 = pq.front().second.second;
             pq.pop();
             if( (r1==0 || c1==0 ||r1 == rsize-1 || c1 == csize-1) && (r1!= entrance[0] || c1 != entrance[1])) return stops;
             for( int i=0; i<4;i++)
@@ -24,11 +24,8 @@ public:
 
                 if( nrow >= 0 && nrow < rsize && ncol >= 0 && ncol <csize && maze[nrow][ncol] != '+')
                 {
-                    if( reached[nrow][ncol] > stops+1)
-                    {
-                        reached[nrow][ncol]=stops+1;
-                        pq.push( {stops+1,{nrow,ncol}});
-                    }
+                    pq.push( {stops+1,{nrow,ncol}});
+                    maze[nrow][ncol]='+';
                 }
             }
         }
