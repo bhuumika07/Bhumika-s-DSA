7a56877ec48eaa19b93c6b07cda64db772bff720
bhuumika07 <bhumikaa2005@gmail.com>
2026-08-21 17:14:10 +0530

    Time: 0 ms (100%), Space: 13 MB (97.81%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,42 +1,52 @@
+class Disjoint{
+    public:
+    vector<int>size;
+    vector<int>parent;
+    Disjoint( int n)
+    {
+        size.resize(n+1 , 1);
+        parent.resize(n+1);
+        for( int i=1; i<=n;i++) parent[i]=i;
+    }
+    int findParent( int node)
+    {
+        if( node == parent[node]) return node;
+        return parent[node]= findParent( parent[node] );
+    }
+
+    void unionBySize( int a , int b)
+    {
+        int ulp_a = findParent(a);
+        int ulp_b = findParent(b);
+        if( ulp_a == ulp_b ) return;
+        int sizea = size[ulp_a];
+        int sizeb = size[ulp_b];
+        if( sizea < sizeb)
+        {
+            parent[ ulp_a] = ulp_b;
+            sizeb+=sizea;
+        }
+        else if( sizeb <= sizea)
+        {
+            parent[ulp_b] = ulp_a;
+            sizea+=sizeb;
+        }
+    }
+};
 class Solution {
 public:
-    bool bfs( int node , int target ,vector<vector<int>>&adj , int n)
-    {
-        vector<bool>visited(n+1,0);
-        queue<int>q;
-        q.push(node);
-        visited[node]=1;
-
-        while(!q.empty())
-        {
-            int start = q.front();
-            q.pop();
-            if( start == target) return 1;
-            for( auto it : adj[start])
-            {
-                if(!visited[it])
-                {
-                    visited[it]=1;
-                    q.push( it );
-                }
-            }
-        }
-        return 0;
-    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n=edges.size();
-        vector<vector<int>>adj(n+1);
-        for(int i=0; i<n; i++)
+        int n = edges.size();
+        Disjoint dj(n);
+        for( int i=0; i<n;i++)
         {
             int u = edges[i][0];
             int v = edges[i][1];
 
-            if( bfs(u,v,adj,n)) return {u,v};
-
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        
+            if( dj.findParent(u) == dj.findParent(v)) return {u,v};
+            dj.unionBySize(u,v);
         }
         return {};
+        
     }
 };
