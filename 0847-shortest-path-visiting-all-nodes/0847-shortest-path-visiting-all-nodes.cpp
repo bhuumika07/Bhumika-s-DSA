9b881cc352c09eb2ba91e95645babcb4120ac130
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-02 17:06:08 +0530

    Time: 11 ms (60.48%), Space: 13.5 MB (69.3%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
@@ -3,25 +3,25 @@ public:
     int shortestPathLength(vector<vector<int>>& graph) {
         int n = graph.size();
         int mask = (1 << n) - 1;
-
+        vector< vector<bool>> visited( n , vector<bool>(1<<n , false));
         queue< tuple<int,int,int> > q;
         for( int i=0; i<n; i++)
         {
             q.push({i,0,1<<i});
+            visited[i][1<<i]=1;
         }
-        vector< vector<bool>> visited( n , vector<bool>(1<<n , false));
+      
         while(!q.empty())
         {
             auto [node , steps , counting] = q.front();
             q.pop();
             if( counting == mask) return steps;
-            if(visited[node][counting]) continue;
-            visited[node][counting]=1;
             for( auto it : graph[node])
             {
                 int state = counting | 1 << it;
                 if( visited[it][state]) continue;
                 q.push({it,steps+1,state});
+                visited[it][state]=1;
             }
 
         }
