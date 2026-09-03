3df07971a0d05a85eca71796a575b165186629b0
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-03 10:36:22 +0530

    Time: 3 ms (64.24%), Space: 8.3 MB (11.6%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    int func( int n , vector<int>&dp)
-    {
-        if( n==1) return 1;
-        if( n <=0 ) return 0;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = func(n-1,dp) + func(n-2,dp);
-    }
     int fib(int n) {
-        vector<int>dp(n+1,-1);
-        return func(n,dp);
+    // for the tabulation solution we start from the edge case itself , is a bottom top approach - opposite to the recursive one which is top to bottom .
+    // we start the process for the the edge case itself-- the point where we were stopping before , now we will be starting from their only.
+    if(n == 0 || n == 1) return n; 
+    vector<int>dp(n+1,-1);
+    dp[0]=0;
+    dp[1]=1;
+    for( int i=2; i<=n;i++)
+    {
+        dp[i]=dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+
+        
     }
 };
