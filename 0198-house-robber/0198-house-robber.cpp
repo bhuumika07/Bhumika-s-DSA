edd120924de21d04aa874f8dd44c819a100de616
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-04 14:03:25 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int dp[100004];
+    int dp[100005];
     int rob(vector<int>& nums) {
         int size = nums.size();
-        if( size == 1) return nums[0];
-        if( size == 2) return max( nums[0] , nums[1]);
-        // memset(dp,-1,sizeof(dp));
-        int backBy3=nums[0];
-        int backBy2=nums[1];
-        int ans = nums[0] + nums[2];
-        int prev=0;
-        for( int i=3;i<nums.size();i++)
+        if(size == 1) return nums[0];
+        if( size == 2) return max(nums[0] , nums[1]);
+        // tabulation solution
+        // i will not rob the house if my (i-1)th robbery has a higher value than my (i-2)th robbery + the amount i am going to get from ith house .
+        memset(dp,-1,sizeof(dp));
+        dp[0]=nums[0];
+        dp[1]=max(nums[1],nums[0]);
+        for( int i=2;i<size;i++)
         {
-           prev = ans;
-           ans = max( backBy2 , backBy3 ) + nums[i];
-           backBy3 = backBy2;
-           backBy2 =prev;
-
+            dp[i] = max(dp[i-1] , dp[i-2] + nums[i]);   
         }
-        return max(prev,max(ans, nums[1]));
-       
+        return dp[size-1];
+        
+        
     }
 };
