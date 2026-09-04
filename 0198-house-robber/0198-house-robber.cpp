4cff7e5b1f8603ba59535194847dae8b583bb22e
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-04 11:16:44 +0530

    Time: 0 ms (100%), Space: 10.4 MB (71.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-    int dp[100005];
-    int doit( vector<int>&nums , int idx )
-    {
-        if( idx < 0) return 0;
-        if (dp[idx] != -1) return dp[idx];
-        // either i could take this one / or the element next to it.
-        int choice1 = nums[idx];
-        if( idx-2 >= 0)
-        {
-            if(dp[idx-2]!=-1) choice1+=dp[idx-2];
-            else choice1+=doit(nums, idx-2);
-        }
-        int choice2=0;
-        if(idx>=1) {choice2 = nums[idx-1] ;
-        if( idx-3 >= 0)
-        {
-            if(dp[idx-3] != -1) choice2 += dp[idx-3];
-            else choice2+=doit(nums,idx-3);
-        }
-        
-        }
-        return dp[idx] = max(choice1, choice2);
-
-    }
+    int dp[100004];
     int rob(vector<int>& nums) {
         int size = nums.size();
+        if( size == 1) return nums[0];
+        if( size == 2) return max( nums[0] , nums[1]);
         memset(dp,-1,sizeof(dp));
-        return doit(nums,size-1);
+        dp[0]=nums[0];
+        dp[1]=nums[1];
+        dp[2]=nums[0]+nums[2];
+        for( int i=3;i<nums.size();i++)
+        {
+            dp[i] = max( dp[i-2] , dp[i-3]) + nums[i];
+        }
+        return max(dp[size-1], dp[size-2]);
        
     }
 };
