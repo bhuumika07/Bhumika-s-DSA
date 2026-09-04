d59b3de50b5b8ac1a18a7b8f1741837b45eb5e24
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-04 11:32:07 +0530

    Time: 0 ms (100%), Space: 10.6 MB (64.98%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,15 +5,20 @@ public:
         int size = nums.size();
         if( size == 1) return nums[0];
         if( size == 2) return max( nums[0] , nums[1]);
-        memset(dp,-1,sizeof(dp));
-        dp[0]=nums[0];
-        dp[1]=nums[1];
-        dp[2]=nums[0]+nums[2];
+        // memset(dp,-1,sizeof(dp));
+        int backBy3=nums[0];
+        int backBy2=nums[1];
+        int ans = nums[0] + nums[2];
+        int prev=0;
         for( int i=3;i<nums.size();i++)
         {
-            dp[i] = max( dp[i-2] , dp[i-3]) + nums[i];
+           prev = ans;
+           ans = max( backBy2 , backBy3 ) + nums[i];
+           backBy3 = backBy2;
+           backBy2 =prev;
+
         }
-        return max(dp[size-1], dp[size-2]);
+        return max(prev,max(ans, nums[1]));
        
     }
 };
