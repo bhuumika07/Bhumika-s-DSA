e816d53b3dc95e8924733f1b9ae9e40448d6a071
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-05 11:37:20 +0530

    Time: 0 ms (100%), Space: 10.7 MB (61.14%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
     int dp[100005];
-    int  doit (vector<int>&nums , int idx , bool takeFirst)
-    {
-       int end = 0;
-       if( !takeFirst) end=1;
-       if( idx < end) return 0; 
-       if( dp[idx] != -1) return dp[idx];
-       int left = nums[idx];
-       if( idx -2 >= end ) 
-       {
-        if(dp[idx-2] != -1) left += dp[idx-2];
-        else left+= doit(nums , idx-2 , takeFirst);
-       }
-       int right=0;
-       if( idx-1 >= end )
-       {
-        right+=nums[idx-1];
-       }
-       if( idx-3 >= end )
-       {
-        if( dp[idx-3] != -1) right +=dp[idx-3];
-        else right+=doit(nums , idx-3 , takeFirst);
-       }
-       return dp[idx] = max(left, right);
-    }
     int rob(vector<int>& nums) {
-        // i will not consider the last house for time being.
         int size = nums.size();
-        if( size == 1) return nums[0];
-        memset( dp , -1 , sizeof(dp));
-        int amount1 = doit( nums , size-2, 1 );
-        memset(dp , -1 , sizeof(dp));
-        int amount2 = doit( nums, size-1, 0);
-        return max(amount1, amount2);
-    }
 
+        if( size <= 3 ) return *max_element(nums.begin() , nums.end());
+
+        memset( dp , -1 , sizeof(dp));
+        // i will consider the first house.
+        dp[0] = nums[0];
+        dp[1] = max( nums[0] , nums[1]);
+        for( int i=2; i<size-1;i++)
+        {
+            dp[i] = max( dp[i-1] , dp[i-2] + nums[i]);
+        }
+        int amount1 = dp[size-2];
+        cout<<amount1<<endl;
+        memset( dp , -1 , sizeof(dp));
+        // i will consider the first house
+        dp[0] = 0;
+        dp[1]=nums[1];
+        dp[2] = max( nums[1] ,nums[2]);
+        for( int i=3; i<size;i++)
+        {
+            dp[i] = max( dp[i-1] , dp[i-2] + nums[i]);
+        }
+        int amount2 = dp[size-1];
+        cout<<amount2;
+        return max( amount1, amount2);
+
+    }
 };
