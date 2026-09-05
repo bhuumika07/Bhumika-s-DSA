62c36a3895bd84aba0eb2f056b008f507668e178
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-05 11:47:17 +0530

    Time: 0 ms (100%), Space: 10.2 MB (75.14%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    int dp[100005];
+    
     int rob(vector<int>& nums) {
         int size = nums.size();
 
         if( size <= 3 ) return *max_element(nums.begin() , nums.end());
 
-        memset( dp , -1 , sizeof(dp));
-        // i will consider the first house.
-        dp[0] = nums[0];
-        dp[1] = max( nums[0] , nums[1]);
+        int secondLast= nums[0];
+        int last = max( nums[0] , nums[1]);
+        int ans1=0;
         for( int i=2; i<size-1;i++)
         {
-            dp[i] = max( dp[i-1] , dp[i-2] + nums[i]);
+            ans1 = max( last , secondLast + nums[i]);
+            secondLast = last;
+            last=ans1;
         }
-        int amount1 = dp[size-2];
-        cout<<amount1<<endl;
-        memset( dp , -1 , sizeof(dp));
-        // i will consider the first house
-        dp[0] = 0;
-        dp[1]=nums[1];
-        dp[2] = max( nums[1] ,nums[2]);
+      
+        secondLast=nums[1];
+        last= max( nums[1] ,nums[2]);
+        int ans2=0;
         for( int i=3; i<size;i++)
         {
-            dp[i] = max( dp[i-1] , dp[i-2] + nums[i]);
+            ans2= max(last ,secondLast+ nums[i]);
+            secondLast = last;
+            last = ans2;
         }
-        int amount2 = dp[size-1];
-        cout<<amount2;
-        return max( amount1, amount2);
+      
+        return max( ans1,ans2);
 
     }
 };
