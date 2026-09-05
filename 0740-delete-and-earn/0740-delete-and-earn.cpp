381307046bf5cd0bb7753b0ad115772c08dbe2b9
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-05 16:13:37 +0530

    Time: 0 ms (100%), Space: 17.3 MB (32.89%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,64 +1,30 @@
 class Solution {
 public:
     int dp[100005];
-    int doit( vector<int>&temp , unordered_map<int,int>&freq, int idx)
-    {
-        if( idx < 0) return 0;
-        if(dp[idx] != -1) return dp[idx];
-
-        // take it
-        int left = temp[idx] * freq[temp[idx]];
-        if(idx-1 >= 0)
+    int deleteAndEarn(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        for(int x: nums) freq[x]++;
+        vector<int>temp;
+        for(auto x:freq) temp.push_back(x.first);
+        sort(temp.begin(), temp.end());
+        if( temp.size() == 1) return temp[0]*freq[temp[0]];
+        
+        dp[0] = temp[0] * freq[temp[0]];
+        if(temp[0]+1 == temp[1]) dp[1] = max(dp[0] , temp[1]*freq[temp[1]]);
+        else dp[1] = dp[0] + temp[1]*freq[temp[1]];
+        for(int i=2; i<temp.size();i++)
         {
-            if(temp[idx-1] +1 == temp[idx])
+            int val = temp[i]*freq[temp[i]];
+            if(temp[i] != temp[i-1] + 1)
             {
-                if( idx -2 >=0  ) 
-                    if (dp[idx-2] != -1)left+=dp[idx-2];
-                    else left+=doit(temp,freq,idx-2);
+                dp[i] = max( dp[i-1] ,dp[i-2]) + val;
             }
             else
             {
-                if( dp[idx-1] != -1) left+=dp[idx-1];
-                else left+=doit(temp,freq,idx-1);
+                dp[i]=max( dp[i-1] , val + dp[i-2]);
             }
         }
+        return dp[temp.size()-1];
 
-        int right=0;
-        if( idx >= 1)
-        {
-            right += temp[idx-1] * freq[temp[idx-1]];
-            if( idx - 2 >= 0 )
-            {
-                if(temp[idx-1] == temp[idx-2]+1)
-                {
-                    if( idx-3 >=0 ) 
-                        if( dp[idx-3] != -1)right+=dp[idx-3];
-                        else right += doit(temp,freq,idx-3);
-                }
-                else
-                {
-                    if(dp[idx-2] != -1) right+=dp[idx-2];
-                    else right+=doit(temp,freq,idx-2);
-                }
-            }
-        }
-
-        return dp[idx]= max(left, right);
-    }
-    int deleteAndEarn(vector<int>& nums) {
-        unordered_map<int,int>freq;
-        for( int x : nums) freq[x]++;
-      
-        vector<int>temp;
-        for( auto x : freq)
-        {
-            temp.push_back(x.first);
-        }
-        sort(temp.begin() , temp.end());
-        int size = temp.size();
-        memset(dp, -1, sizeof(dp));
-        return doit(temp,freq,size-1);
-
-        
     }
 };
