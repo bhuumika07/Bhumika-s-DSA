8550f715516122574a0a2025a84c7fb65650cb42
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-09 18:53:00 +0530

    Time: 0 ms (100%), Space: 9.3 MB (21.25%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -7,18 +7,13 @@ public:
        if( idx == s.size()) return 1;
        if( s[idx] == '0') return 0;
        if( dp[idx] != -1) return dp[idx];
-       int singlee = 0;
-       if( dp[idx+1] != -1) singlee = dp[idx+1];
-       else singlee = doit( s, idx+1);
+       int singlee = doit( s, idx+1);
        int doublee=0;
        if( idx+1 < s.size())
        {
             int num =s[idx]-'0';
-            int num1=num;
             num = num*10 + s[idx+1] -'0';
-            if(num1!=0 &&  num <= 26 )  
-                if( dp[idx+2] !=-1 ) doublee = dp[idx+2];
-                else doublee = doit( s, idx+2);;
+            if(num <= 26 )  doublee = doit( s, idx+2);;
         } 
     
         return dp[idx] = singlee + doublee;
