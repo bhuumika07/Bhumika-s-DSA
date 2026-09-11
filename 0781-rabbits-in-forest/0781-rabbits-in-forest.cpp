f400087de85eef1dfa101ed21b8cc2c02f666232
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-11 22:50:19 +0530

    Time: 1 ms (32.44%), Space: 12.2 MB (37.16%) - LeetHub

--- 0781-rabbits-in-forest/0781-rabbits-in-forest.cpp
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.cpp
@@ -8,18 +8,12 @@ public:
         {
             int x = it.first;
             int y = it.second;
-            // if( x == 0) count+=y;
-            // else if( y <= x) count+= x+1;
-            // else
-            // {
-               if( y%(x+1) == 0) count+=y;
-               else
-               {
+            if( y%(x+1) == 0) count+=y;
+            else
+            {
                 int rem = y%(x+1);
                 count+= y + (x+1-rem);
-               }
-            // }
-            
+            }
         }
         return count;
         
