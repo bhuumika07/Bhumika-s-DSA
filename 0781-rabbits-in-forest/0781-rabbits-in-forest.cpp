a14679e5322e823a778aee0be91082eaefeb1eff
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-11 22:48:57 +0530

    Time: 0 ms (100%), Space: 12.1 MB (67.07%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        unordered_map<int,int>freq;
+        for( auto x : answers) freq[x]++;
+        int count=0;
+        for( auto it : freq)
+        {
+            int x = it.first;
+            int y = it.second;
+            if( x == 0) count+=y;
+            else if( y <= x) count+= x+1;
+            else
+            {
+               if( y%(x+1) == 0) count+=y;
+               else
+               {
+                int rem = y%(x+1);
+                count+= y + (x+1-rem);
+               }
+            }
+            
+        }
+        return count;
+        
+    }
+};
