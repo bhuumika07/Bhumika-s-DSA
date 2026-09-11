5fbd5a7614d91bc450e4fd6ad7467607485bd379
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-11 14:58:23 +0530

    Time: 3 ms (72.1%), Space: 10.9 MB (80.69%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
-    unordered_set<string>contains;
-    int dp[301];
-    bool doit( string&s , int idx)
-    {
-        if( idx == s.size()) return 1;
-        string formed="";
-        if(dp[idx] != -1) return dp[idx];
-        for( int i=idx ; i<s.size(); i++)
-        {
-            formed+=s[i];
-            if(contains.find(formed) != contains.end())
-            {
-                if(doit(s,i+1)) return dp[idx] = 1;
-            }
-        }
-        return dp[idx] = 0;
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
-        memset(dp,-1,sizeof(dp));
+        // tabulation approach.
+        // starting condition would be the base case of memoization
+        // i.e  when it reaches the end of the string
+        unordered_set<string>contains;
         for( string &word : wordDict) contains.insert(word);
-        return doit(s,0);
+        int dp[305];
+        memset(dp , -1, sizeof(dp));
+        int size = s.size();
+        dp[size]=1;
+        for( int i = size-1; i>=0; i--)
+        {
+            string p="";
+            bool found=0;
+            for( int j=i; j<size;j++)
+            {
+                p+=s[j];
+                if(contains.find(p) != contains.end())
+                {
+                    if(dp[j+1] == 1)
+                    {
+                        found=1;
+                        break;
+                    }
+                }
+            }
+            if(found) dp[i]=1;
+            else dp[0]=0;
+        }
+        return dp[0];
+
         
     }
 };
