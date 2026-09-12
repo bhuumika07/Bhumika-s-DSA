ebf81cd8c8f5c28720d15e852b1c064dd566e0c3
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-12 11:24:34 +0530

    Time: 99 ms (34.73%), Space: 103.7 MB (89.16%) - LeetHub

---
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long dp[100005];
+    long long doit( vector<vector<int>>&rides , int idx )
+    {
+        if(idx == rides.size()) return 0;
+        if( dp[idx] != -1 ) return dp[idx];
+        long long dont = doit(rides , idx+1);
+        long long take = rides[idx][1] - rides[idx][0] + rides[idx][2];
+        int next = rides.size();
+        int low = idx+1; int high = rides.size()-1;
+        while( low <= high)
+        {
+            int mid = low + (high-low)/2;
+            if( rides[mid][0] >= rides[idx][1])
+            {
+                next = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        take += doit( rides , next);
+        return dp[idx]=  max(take,dont);
+    }
+    long long maxTaxiEarnings(int n, vector<vector<int>>& rides) {
+        memset( dp ,-1 , sizeof(dp));
+        sort(rides.begin() , rides.end());
+        return doit(rides,0);
+
+        
+    }
+};
