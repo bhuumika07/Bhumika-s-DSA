d7cde9b01a6300c96509d92e36e5829d6d9b3361
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-16 15:56:12 +0530

    Time: 1386 ms (8.67%), Space: 9.8 MB (76.02%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    int maxPalindromes(string s, int k) {
-        int n = s.size();
-        if (k == 1) return n;
-
-        int res = 0;
-
-        for (int i = 0; i <= n - k;) {
-            int step = 1;
-
-            for (int d : {k, k + 1}) {
-                if (i + d <= n && equal(s.begin() + i, s.begin() + i + (d >> 1),
-                                        s.rbegin() + (n - (i + d)))) {
-                    step = d;
-                    res++;
-                    break;
-                }
-            }
-
-            i += step;
+    int dp[2001];
+   bool checkit(string &s, int &left, int &right)
+    {
+        int end = left + right;
+        if (end > s.size()) return 0;
+        for (int i = left; i < left + right / 2; i++)
+        {
+            if (s[i] != s[end - 1 - (i - left)])
+            return 0;
         }
+        return 1;
+    }
+    int solve( string &s , int start , int k)
+    {
+        if(start + k >  s.size()) return 0;
+        if( dp[start] != -1) return dp[start];
+        int take=0;
+        int dont= 0;
+         for(int i= k; start + i <= s.size(); i++)
+        {
+            if(checkit(s, start, i))
+            {
+                take = max(take, 1 + solve(s, start + i, k));
+            }
+        }
+        dont = solve(s, start+1 , k);
 
-        return res;
+        return dp[start] = max( take , dont);
+    }
+    int maxPalindromes(string s, int k) {
+        if(k == 1) return s.size();
+        memset( dp , -1 , sizeof(dp));
+        return solve( s , 0 , k);
+        
     }
 };
