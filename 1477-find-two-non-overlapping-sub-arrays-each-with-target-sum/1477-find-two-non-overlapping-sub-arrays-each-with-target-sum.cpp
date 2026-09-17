c75c526d7e5e9e2aa43c07e5b34830ed39bc125b
bhuumika07 <bhumikaa2005@gmail.com>
2026-09-17 18:12:15 +0530

    Time: 2 ms (95.88%), Space: 88.9 MB (79.38%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     int minSumOfLengths(vector<int>& arr, int target) {
-
         int n = arr.size();
-
-        vector<int> best(n, INT_MAX);
+        vector<int> ans( n , INT_MAX);
 
         int left = 0;
-        long long sum = 0;
-        int ans = INT_MAX;
+        int right =0;
+        int sum=0;
+        int ans1 = INT_MAX;
+        while( right < n)
+        {
+            sum+=arr[right];
 
-        for (int right = 0; right < n; right++) {
-
-            sum += arr[right];
-
-            while (sum > target) {
-                sum -= arr[left];
+            while( sum > target)
+            {
+                sum-=arr[left];
                 left++;
             }
-
-            // Carry forward previous best
-            if (right > 0)
-                best[right] = best[right - 1];
-
-            if (sum == target) {
-
+            if( right > 0) ans[right] = ans[right-1];
+            if( sum == target)
+            {
                 int len = right - left + 1;
 
-                // Previous subarray must end before 'left'
-                if (left > 0 && best[left - 1] != INT_MAX) {
-                    ans = min(ans, len + best[left - 1]);
+                if( left > 0 && ans[left-1] != INT_MAX)
+                {
+                    ans1 = min( ans1, len + ans[left-1]);
                 }
 
-                // Current subarray becomes the best up to 'right'
-                best[right] = min(best[right], len);
+                ans[right] = min( ans[right] , len);
             }
-        }
 
-        return ans == INT_MAX ? -1 : ans;
+            right++;
+        }
+        return (ans1==INT_MAX ? -1 : ans1);
+        
     }
 };
